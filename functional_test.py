aa34b4eada3f9f3d4358361a70bb1ad029dcbe48
Ty4 <tdavidso@ualberta.ca>
2014-06-26 14:41:53 -0600

    Started working on the home page, writing tests.

--- functional_test.py
+++ functional_test.py
@@ -12,83 +12,86 @@ class NewVisitorTest(unittest.TestCase):
     def tearDown(self):
         self.browser.quit()
         
-    def test_can_register_as_a_seller_and_log_in(self):
-        self.fail("Finish this test!")
-        # Dot wants to sell her car, and her buyer wants to see
-        # that she's listed it on NoLemon.ca. She goes there to
-        # list it
-        
-        # Dot notices at the top of the page, there is text that
-        # tells her she should register for an account to sell
-        # her car
-        
-        # Dot clicks on the register link and it takes her to the
-        # registration page
-        
-        # Dot inputs her name and her email address. The page
-        # tells her that she will be sent a temporary password
-        # to her email, and in the future she can use her email
-        # address to log in to NoLemon.ca
-        
-        # After Dot checks her email and receives her temporary
-        # password, she returns to NoLemon via the link in her email
-        # where she is prompted to change her password.
-        
-        # She inputs her chosen password and hits save, where she is
-        # then taken her account page. 
-        
-        # She wants to test that it worked so the logs out of NoLemon
-        # and logs back in. 
-        
-        # When she logs in she notices she is taken back to the main
-        # page, but in the top right corner she sees her name and a
-        # link to her account page
-        
-        # satisfied, Dot logs out and closes her browser
-        
+#     def test_can_register_as_a_seller_and_log_in(self):
+#         self.fail("Finish this test!")
+#         # Dot wants to sell her car, and her buyer wants to see
+#         # that she's listed it on NoLemon.ca. She goes there to
+#         # list it
+#         
+#         # Dot notices at the top of the page, there is text that
+#         # tells her she should register for an account to sell
+#         # her car
+#         
+#         # Dot clicks on the register link and it takes her to the
+#         # registration page
+#         
+#         # Dot inputs her name and her email address. The page
+#         # tells her that she will be sent a temporary password
+#         # to her email, and in the future she can use her email
+#         # address to log in to NoLemon.ca
+#         
+#         # After Dot checks her email and receives her temporary
+#         # password, she returns to NoLemon via the link in her email
+#         # where she is prompted to change her password.
+#         
+#         # She inputs her chosen password and hits save, where she is
+#         # then taken her account page. 
+#         
+#         # She wants to test that it worked so the logs out of NoLemon
+#         # and logs back in. 
+#         
+#         # When she logs in she notices she is taken back to the main
+#         # page, but in the top right corner she sees her name and a
+#         # link to her account page
+#         
+#         # satisfied, Dot logs out and closes her browser
+#         
 
-    def test_seller_can_request_an_inspection(self):
-        self.fail("Finish this test!")
-        
-        # Dot remembers she wanted to get an inspection done for
-        # her vehicle, so she goes to NoLemon.ca, and logs in to
-        # her account
-        
-        # She clicks on the link in the top right corner to go to
-        # her account page
-        
-        # From her account page, she notices text that asks if she
-        # has a car to sell. Beside it is a button that she can
-        # click to request an inspection by a mechanic.
-        
-        # Dot clicks the button and is taken to a page that tells
-        # her she needs to fill out a request form, and pay an
-        # inspection fee.
-        
-        # She fills out the form with the vehicle's VIN, year, 
-        # make, and model. At the bottom there's text that tells her
-        # when she clicks okay, a pop-up will come up asking for
-        # her credit card information.
-        
-        # Dot enters her information, and when the pop-up closes,
-        # she is brought to a page with a thank you message. The 
-        # page also tells her that a download will start soon
-        # with the form that she will need to take to the mechanic
-        
-        # Dot notices the page lists the mechanics that are
-        # registered on the site, with their contact information 
-        # and location.
-        
-        # After she choses a mechanic, Dot closes her browser.
-        
-        
+#     def test_seller_can_request_an_inspection(self):
+#         self.fail("Finish this test!")
+#         
+#         # Dot remembers she wanted to get an inspection done for
+#         # her vehicle, so she goes to NoLemon.ca, and logs in to
+#         # her account
+#         
+#         # She clicks on the link in the top right corner to go to
+#         # her account page
+#         
+#         # From her account page, she notices text that asks if she
+#         # has a car to sell. Beside it is a button that she can
+#         # click to request an inspection by a mechanic.
+#         
+#         # Dot clicks the button and is taken to a page that tells
+#         # her she needs to fill out a request form, and pay an
+#         # inspection fee.
+#         
+#         # She fills out the form with the vehicle's VIN, year, 
+#         # make, and model. At the bottom there's text that tells her
+#         # when she clicks okay, a pop-up will come up asking for
+#         # her credit card information.
+#         
+#         # Dot enters her information, and when the pop-up closes,
+#         # she is brought to a page with a thank you message. The 
+#         # page also tells her that a download will start soon
+#         # with the form that she will need to take to the mechanic
+#         
+#         # Dot notices the page lists the mechanics that are
+#         # registered on the site, with their contact information 
+#         # and location.
+#         
+#         # After she choses a mechanic, Dot closes her browser.
+#         
+#         
     def test_can_search_for_car_inspection_video(self):
         # Enzo browses to NoLemon.ca
         self.browser.get('http://localhost:8080')
-        self.fail("Finish the test!")
         
         # Enzo notices the title is NoLemon, and at the top
         # of the page, there is a header that also says NoLemon
+        self.assertIn("NoLemon", self.browser.title)
+        header = self.browser.find_element_by_tag_name("h1")
+        self.assertIn("NoLemon", header)
+        self.fail("Finish the test!")
         
         # Enzo sees that there is a search bar that invites him
         # to search for a vehicle using it's VIN
@@ -111,9 +114,9 @@ class NewVisitorTest(unittest.TestCase):
         # rating, and closes his browser
         
     
-    def test_mechanic_can_upload_inspection(self):
-        self.fail("Finish this test!")
-        
+#     def test_mechanic_can_upload_inspection(self):
+#         self.fail("Finish this test!")
+#         
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
--- inspections/tests.py
+++ inspections/tests.py
@@ -9,11 +9,11 @@ from inspections.views import home_page
 class HomePageTest(TestCase):
     
     def test_root_url_resolves_to_home_page_view(self):
-        found = resolve('/')
-        self.assertEqual(found.func, home_page)
-        
+         found = resolve('/')
+         self.assertEqual(found.func, home_page)
+         
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()
         response = home_page(request)
-        expected_html = render_to_string('home.html')
+        expected_html = render_to_string('index.html')
         self.assertEqual(response.content.decode(), expected_html)
