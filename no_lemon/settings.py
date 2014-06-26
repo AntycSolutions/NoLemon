06c7cd65ee2348103a188233913ae37584cd4e79
Tyler Davidson <tdavidso@ualberta.ca>
2014-06-26 13:55:52 -0600

    added staticfiles list to settings

--- no_lemon/settings.py
+++ no_lemon/settings.py
@@ -82,3 +82,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "assets"),
+    os.path.join(BASE_DIR, "documentation")
+)
