33e15005318ee8fcb9a89b3573519deb5be8f48b
Solanar <and_555@hotmail.com>
2014-07-11 21:15:45 -0600

    formatting and site statistics

--- no_lemon/urls.py
+++ no_lemon/urls.py
@@ -4,33 +4,38 @@ from django.contrib import admin
 from inspections.front.authenticationViews import RegisterSellerView, Login, \
     Logout, RegisterCustomerView
 from inspections.views import VehicleDetail, VehicleList
+from inspections.front.statistics import Statistics
 admin.autodiscover()
 
 
-vehiclepatterns = patterns('',
-                           url(r'^$', VehicleList.as_view(),
-                               name='vehicle_list'),
-                           url(r'^(?P<vin>[0-9a-hj-npr-z]{1,17})',
-                               VehicleDetail.as_view(),
-                               name='vehicle_detail'),
-                           )
+vehiclepatterns = patterns(
+    '',
+    url(r'^$', VehicleList.as_view(),
+        name='vehicle_list'),
+    url(r'^(?P<vin>[0-9a-hj-npr-z]{1,17})',
+        VehicleDetail.as_view(),
+        name='vehicle_detail'),
+    )
 
-registrationpatterns = patterns('',
-                                url(r'^seller$',
-                                    RegisterSellerView.as_view(),
-                                    name='register_seller'),
-                                url(r'^customer$',
-                                    RegisterCustomerView.as_view(),
-                                    name='register_customer'),
-                                )
+registrationpatterns = patterns(
+    '',
+    url(r'^seller$',
+        RegisterSellerView.as_view(),
+        name='register_seller'),
+    url(r'^customer$',
+        RegisterCustomerView.as_view(),
+        name='register_customer'),
+    )
 
-urlpatterns = patterns('',
-                       # Examples:
-                       url(r'^$', 'inspections.views.home_page', name='home'),
+urlpatterns = patterns(
+    '',
+    # Examples:
+    url(r'^$', 'inspections.views.home_page', name='home'),
 
-                       url(r'^admin/', include(admin.site.urls)),
-                       url(r'^register/', include(registrationpatterns)),
-                       url(r'^login/$', Login.as_view(), name='login'),
-                       url(r'^logout/$', Logout.as_view(), name='logout'),
-                       url(r'^vehicles/', include(vehiclepatterns)),
-                       )
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^register/', include(registrationpatterns)),
+    url(r'^login/$', Login.as_view(), name='login'),
+    url(r'^logout/$', Logout.as_view(), name='logout'),
+    url(r'^vehicles/', include(vehiclepatterns)),
+    url(r'^statistics/', Statistics.as_view(), name='statistics')
+    )
