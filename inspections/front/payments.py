3bee355f994105202ac2b40e91842885e7a92ebf
Ty4 <tdavidso@ualberta.ca>
2014-07-25 14:10:44 -0600

    Added NL logo to Stripe window

--- inspections/front/payments.py
+++ inspections/front/payments.py
@@ -1,26 +1,48 @@
+from django import forms
 from django.core.urlresolvers import reverse
 from django.http.response import Http404, HttpResponsePermanentRedirect
-from django.views.generic import TemplateView
+from django.views.generic.edit import CreateView
 import stripe
 
-from inspections.models import Vehicle
+from inspections.forms.request_inspection import RequestInspectionForm
+from inspections.models import RequestInspection
 
 
-class PaymentView(TemplateView):
+class PaymentView(CreateView):
 
+    model = RequestInspection
+    success_url = '/vehicles/'
+    form_class = RequestInspectionForm
     template_name = 'testpayment.html'
 
     def get(self, request, *args, **kwargs):
+        self.object = None
+        kwargs['vehicle'] = request.GET['vehicle']
         context = self.get_context_data(**kwargs)
-        vin = request.GET['vehicle']
-        vehicle = Vehicle.objects.get(vin=vin)
-        context['vehicle'] = vehicle
+
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+
+        context['form'] = form
+
+        print(request.GET)
+
         return self.render_to_response(context)
+
+    def get_context_data(self, **kwargs):
+        context = super(PaymentView, self).get_context_data(**kwargs)
+        return context
+
+    def get_form(self, form_class):
+        form = form_class(**self.get_form_kwargs())
+        form.fields['seller'].widget = forms.HiddenInput()
+        form.fields['vehicle'].widget = forms.HiddenInput()
+        form.fields['request_date'].widget = forms.HiddenInput()
+        return form
 
     def post(self, request, *args, **kwargs):
         vin = request.POST['vehicle']
-        # Set your secret key: remember to change this to your live secret key
-        # in production
+        # Set your secret key: remember to change this to your live secret key in production
         # See your keys here https://dashboard.stripe.com/account
         stripe.api_key = "sk_test_y8HvZWWtuKZAMrQsRPtRro6F"
 
@@ -38,7 +60,7 @@ class PaymentView(TemplateView):
             )
         except stripe.CardError as e:
             # The card has been declined
-            print("Error: ", e)
             return Http404()
+        paid = True
         request.session['vehicle'] = vin
         return HttpResponsePermanentRedirect(reverse('request_inspection_create'))
