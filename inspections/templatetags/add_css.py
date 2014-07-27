68cce3a95afc0774771b5daa26a13e1355ce8f34
Solanar <and_555@hotmail.com>
2014-07-27 15:48:48 -0600

    allows editing of a django field's attributes, mainly for setting classes

---
+++ inspections/templatetags/add_css.py
@@ -0,0 +1,20 @@
+# http://vanderwijk.info/blog
+# /adding-css-classes-formfields-in-django-templates/
+
+from django import template
+register = template.Library()
+
+
+@register.filter
+def addcss(field, css):
+    attrs = {}
+    definition = css.split(',')
+
+    for d in definition:
+        if ':' not in d:
+            attrs['class'] = d
+        else:
+            t, v = d.split(':')
+            attrs[t] = v
+
+    return field.as_widget(attrs=attrs)
