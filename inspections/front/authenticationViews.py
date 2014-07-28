a87620a9a687de01870bef8c45d741d8715156eb
Solanar <and_555@hotmail.com>
2014-07-27 19:57:51 -0600

    add update account views, add register mechanic account view

--- inspections/front/authenticationViews.py
+++ inspections/front/authenticationViews.py
@@ -1,12 +1,15 @@
 from django.conf import settings
 from django.contrib.auth import authenticate, login as auth_login, \
     logout as auth_logout
+from django.core.urlresolvers import reverse_lazy
+from django.shortcuts import redirect
 from django.contrib.auth.forms import AuthenticationForm
 from django.http.response import HttpResponseRedirect
-from django.views.generic import FormView, View
+from django.views.generic import FormView, View, UpdateView
 
 from ..forms.registration import SellerCreationForm, \
-    CustomerCreationForm
+    CustomerCreationForm, MechanicCreationForm
+from ..models import Seller, Customer, Mechanic
 
 
 class RegisterSellerView(FormView):
@@ -46,6 +49,30 @@ class RegisterSellerView(FormView):
                                                              url_end='seller'))
 
 
+class UpdateSellerView(UpdateView):
+    template_name = 'testupdate.html'
+    model = Seller
+    fields = ['first_name', 'last_name', ]
+
+    def get_object(self):
+        seller = None
+        try:
+            seller = Seller.objects.get(email=self.request.user.email)
+        except:
+            pass
+        return seller
+
+    def get(self, request):
+        self.object = self.get_object()
+        if not self.object:
+            return redirect('/')
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return self.render_to_response(
+            self.get_context_data(form=form, url_end='seller')
+        )
+
+
 class RegisterCustomerView(FormView):
     template_name = 'testregistration.html'
     form_class = CustomerCreationForm
@@ -82,6 +109,95 @@ class RegisterCustomerView(FormView):
         return self.render_to_response(
             self.get_context_data(form=form,
                                   url_end='customer')
+        )
+
+
+class UpdateCustomerView(UpdateView):
+    template_name = 'testupdate.html'
+    model = Customer
+    fields = ['first_name', 'last_name', ]
+    success_url = reverse_lazy('update_customer')
+
+    def get_object(self):
+        customer = None
+        try:
+            customer = Customer.objects.get(email=self.request.user.email)
+        except:
+            pass
+        return customer
+
+    def get(self, request):
+        self.object = self.get_object()
+        if not self.object:
+            return redirect('/')
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return self.render_to_response(
+            self.get_context_data(form=form, url_end='customer')
+        )
+
+
+class RegisterMechanicView(FormView):
+    template_name = 'testregistration.html'
+    form_class = MechanicCreationForm
+    success_url = '/'
+
+    def form_valid(self, form):
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        user = form.save()
+
+        user = authenticate(
+            email=user.email,
+            password=form.cleaned_data["password1"])  # get plaintext
+        if user is not None:
+            if user.is_active:
+                auth_login(self.request, user)
+                # Redirect to a success page.
+                print("login success")
+            else:
+                # Return a 'disabled account' error message
+                print ("account inactive")
+        else:
+            # Return an 'invalid login' error message.
+            print ("user not authenticated")
+
+        return super(RegisterMechanicView, self).form_valid(form)
+
+    def get(self, request, *args, **kwargs):
+        """
+        Handles GET requests and instantiates a blank version of the form.
+        """
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return self.render_to_response(
+            self.get_context_data(form=form,
+                                  url_end='mechanic')
+        )
+
+
+class UpdateMechanicView(UpdateView):
+    template_name = 'testupdate.html'
+    model = Mechanic
+    fields = ['first_name', 'last_name',
+              'address', 'city', 'province', ]
+
+    def get_object(self):
+        mechanic = None
+        try:
+            mechanic = Mechanic.objects.get(email=self.request.user.email)
+        except:
+            pass
+        return mechanic
+
+    def get(self, request):
+        self.object = self.get_object()
+        if not self.object:
+            return redirect('/')
+        form_class = self.get_form_class()
+        form = self.get_form(form_class)
+        return self.render_to_response(
+            self.get_context_data(form=form, url_end='mechanic')
         )
 
 
