b378031928099f9332a538938d4220b039fbaab5
Solanar <and_555@hotmail.com>
2014-08-01 21:07:23 +0000

    allow apache2 to find the wsgi file via path

--- no_lemon/wsgi.py
+++ no_lemon/wsgi.py
@@ -8,6 +8,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
 import os
+import sys
+sys.path.append('/home/airith/public_html/nolemon.antyc.ca/NoLemon')
 
 from django.core.wsgi import get_wsgi_application
 
