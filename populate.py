37af85649d703a1299c880e9214a888a19199f73
Ty4 <tdavidso@ualberta.ca>
2014-08-12 22:33:44 -0600

    Made a slightly more robust script for populate. It just adds more things.

--- populate.py
+++ populate.py
@@ -1,6 +1,7 @@
 # Populate the database with showcase and testing data
 import datetime
 import os
+import random
 
 
 def populate():
@@ -10,12 +11,15 @@ def populate():
     admin.is_admin = True
     admin.save()
 
+    print("Creating Sellers...")
     bobSeller = add_seller("bob@seller.ca", "Bob", "Bobson", password)
     samSeller = add_seller("sam@seller.ca", "Sam", "Samson", password)
     timSeller = add_seller("tim@seller.ca", "Tim", "Timson", password)
     tomSeller = add_seller("tom@seller.ca", "Tom", "Tomson", password)
     patSeller = add_seller("pat@seller.ca", "Pat", "Patson", password)
+    print("... Sellers added.")
 
+    print("Creating Mechanics...")
     sarahMechanic = add_mechanic(
         "sarah@mechanic.ca", "Sarah", "Sarahson", password, "7801",
         "123 st nw", "edmonton", "ab")
@@ -31,23 +35,79 @@ def populate():
     wendyMechanic = add_mechanic(
         "wendy@mechanic.ca", "Wendy", "Wendyson", password, "7805",
         "23 ave nw", "edmonton", "ab")
+    print("... Mechanics added.")
 
-    aleroVehicle = add_vehicle(
-        "123", bobSeller, "Oldsmobile", "Alero", 2004)
-    lebaronVehicle = add_vehicle(
-        "456", bobSeller, "Chrysler", "Le Baron", 1989)
-    grandcherokeeVehicle = add_vehicle(
-        "789", bobSeller, "Jeep", "Grand Cherokee", 2011)
-
+    print("Creating Vehicles...")
+    make_model_choices = {'Oldsmobile': ['Cutlass',
+                                         '442',
+                                         '88',
+                                         'Alero',
+                                         'Bravada'],
+                          'Chrysler': ['Airflow',
+                                       'Alpine',
+                                       '300',
+                                       'Aspen',
+                                       'Royal'],
+                          'Ford': ['Del Rey',
+                                   'Escort',
+                                   'Fiesta',
+                                   'Crown Victoria',
+                                   'Tempo'],
+                          'Dodge': ['Royal',
+                                    'Shadow',
+                                    'Lancer',
+                                    'Spirit',
+                                    'Ram'],
+                          'Jeep': ['Commander',
+                                   'Liberty',
+                                   'Grand Cherokee',
+                                   'Cherokee',
+                                   'Wrangler'],
+                          'Chevrolet': ['Lanos',
+                                        'Lumina',
+                                        'Cavalier',
+                                        'Impala',
+                                        'Malibu'],
+                          'Mercedes-Benz': ['E-Class',
+                                            'SLS AMG',
+                                            'R230',
+                                            'CLA-Class',
+                                            'W166'],
+                          'Lincoln': ['Continental',
+                                      'Navigator',
+                                      'Mark LT',
+                                      'MKC',
+                                      'MKS']}
+    for x in range(15):
+        vin = random.randint(100,999)
+        owner = random.choice(Seller.objects.all())
+        keys = [key for key in make_model_choices.keys()]
+        make = random.choice(keys)
+        model = random.choice(make_model_choices[make])
+        years = [year for year in range(1950,2014)]
+        year = random.choice(years)
+        
+        vehicle = add_vehicle(vin, owner, make, model, year)
+    print("...Vehicles added.")
+    
+    print("Creating Inspections...")
     now = datetime.datetime.now()
-    goodInspection = add_inspection(
-        0, sarahMechanic, aleroVehicle, "Good", now, 0)
-    duplicateInspection = add_inspection(
-        1, sarahMechanic, aleroVehicle, "Good", now, 0)
-    alrightInspection = add_inspection(
-        2, sarahMechanic, lebaronVehicle, "Alright", now, 0)
-    badInspection = add_inspection(
-        3, sarahMechanic, grandcherokeeVehicle, "Bad", now, 0)
+    comment_choices = ['Good',
+                       'Bad',
+                       'Everything is broken',
+                       'Best vehicle ever.',
+                       'Something is broken.',
+                       'Incredible.',
+                       'Kill this car with fire.',
+                       'Alright']
+    for x in range(15):
+        mechanics = Mechanic.objects.all()
+        mech = random.choice(mechanics)
+        vehicles = Vehicle.objects.all()
+        vehicle = random.choice(vehicles)
+        comment = random.choice(comment_choices)
+        inspection = add_inspection(x-1, mech, vehicle, comment, now, 0)
+    print("... Inspections added.\n")
 
     print_all_sellers()
     print_all_mechanics()
@@ -104,6 +164,13 @@ def add_inspection(pk, mechanic, vehicle, comments, date, views):
 
 
 def add_vehicle(vin, owner, make, model, year):
+    try:
+        vehicle = Vehicle.objects.get(vin=vin)
+        print("Already exists, vehicle:", vehicle)
+        return vehicle
+    except:
+        pass
+        
     vehicle, created = Vehicle.objects.get_or_create(
         vin=vin, owner=owner,
         make=make, model=model, year=year
@@ -136,7 +203,7 @@ def print_all_inspections():
 def print_all_vehicles():
     print("Vehicles:")
     for vehicle in Vehicle.objects.all():
-        print(vehicle)
+        print(vehicle.__str__() + ' ' + vehicle.owner.__str__())
 
 
 if __name__ == '__main__':
