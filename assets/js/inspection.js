ab31977fc2ea2decc7e47d6422cf6d2bd72cbd66
Ty4 <tdavidso@ualberta.ca>
2014-08-13 12:57:23 -0600

    Refined the use of the modal a bit. No major change.

--- assets/js/inspection.js
+++ assets/js/inspection.js
@@ -3,14 +3,13 @@ var vin;
 
 jQuery().ready(function($){
 	$('.inspection_select').click(function(){
+		$('#stripeButton').attr('disabled', 'disabled');
+		$('input[name=option]').attr('checked', false);
 		$('#inspection_modal').fadeIn("slow");
 		$('#inspection_modal').css('pointer-events', 'auto');
 	});
 
-	$('.close').click(function(){
-		$('#inspection_modal').fadeOut("slow");
-		$('#inspection_modal').css('pointer-events', 'none');
-	});
+	$('.close').click(closeModal);
 
 	$('#inspection_modal').keypress(function(e){
 		if (e.keyCode == 27) { 
@@ -18,7 +17,7 @@ jQuery().ready(function($){
 	    }
 	});
 
-	$('input[name=option]').on('change', function(e){
+	$('input[name=option]').on('click', function(e){
 		$('#stripeButton').removeAttr('disabled');
 	});
 
@@ -65,3 +64,10 @@ jQuery().ready(function($){
 	});
 
 });
+
+closeModal = function () {
+	$('#inspection_modal').fadeOut("slow");
+	$('#inspection_modal').css('pointer-events', 'none');
+	$('input[name=option]:checked').attr('checked', false).checkboxradio("refresh");
+	$('#stripeButton').attr('disabled', 'disabled');
+}
