65f9ac38b6eb58fcee425206baa0e604397ecb99
Ty4 <tdavidso@ualberta.ca>
2014-08-14 13:46:14 -0600

    Added a send_email utility function... refactor to follow

--- inspections/utilities.py
+++ inspections/utilities.py
@@ -1,5 +1,8 @@
+from django.core.mail import send_mass_mail
 from django.http.response import Http404
 import stripe
+
+from inspections.models import BaseUser
 
 
 def process_stripe(token, cost):
@@ -24,3 +27,16 @@ def process_stripe(token, cost):
         # The card has been declined
         print("Stripe Error:", e)
         return Http404()
+
+
+def send_email():
+    admin_emails = BaseUser.objects.filter(is_admin=True).values('email')
+    title = "NoLemon - Inspection Request"
+    content = "An inspection request has been made. Please approve.\n"
+
+    datatuple = []
+    for email in admin_emails:
+        datatuple.append((title, content, 'NoLemon <no-reply@nolemon.ca>',
+                          [email['email']]))
+
+    send_mass_mail(datatuple, fail_silently=False)
