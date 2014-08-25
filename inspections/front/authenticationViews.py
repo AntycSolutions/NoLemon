a4e7f0f37eff0a96b98a87210431b9dc4236dbe0
Solanar <and_555@hotmail.com>
2014-08-25 11:39:27 -0600

    model_type var passed to make update.html generic

--- inspections/front/authenticationViews.py
+++ inspections/front/authenticationViews.py
@@ -113,7 +113,8 @@ class UpdateSellerView(UpdateView):
         form_class = self.get_form_class()
         form = self.get_form(form_class)
         return self.render_to_response(
-            self.get_context_data(form=form, url_end='seller')
+            self.get_context_data(form=form, url_end='seller',
+                                  model_type='Account')
         )
 
 
