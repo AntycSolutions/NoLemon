1e041013522f7e7c8cf2d6b8e8e85f8fc960dded
Solanar <and_555@hotmail.com>
2014-09-05 15:42:03 -0600

    fix missing bracket, set focus so you can esc modal, fadeOut instead of hide, change color of selected payment level, use custom stripe button to set payment level depending on users choice, better error msg

--- assets/js/inspection.js
+++ assets/js/inspection.js
@@ -10,73 +10,119 @@ jQuery().ready(function($){
 			window.location = url;
 		}
 
-		$('#stripeButton').attr('disabled', 'disabled');
+		$('#customButton').attr('disabled', 'disabled');
 		$('input[name=payment_level]').attr('checked', false);
 		$('#inspection_modal').fadeIn("slow");
 		$('#inspection_modal').css('pointer-events', 'auto');
 		$('input[name=inspection]').attr('value', id);
-		$('input[type=radio').hide();
+		$('input[type=radio]').hide();
 		$('#id_inspection option[value=' + id + ']').prop('selected', true);
 		$('#inspection_select_form').trigger('reset');
+		$('input[name=name]').focus();
 	});
 
 	$('.close').click(closeModal);
 
 	$('#inspection_modal').keypress(function(e){
-		if (e.keyCode == 27) { 
-	        $("#inspection_modal").hide(500);
+		if (e.keyCode == 27) {
+	        $("#inspection_modal").fadeOut(500);
 	    }
 	});
 
 	$('input[name=payment_level]').on('click', function(e){
-		$('#stripeButton').removeAttr('disabled');
-		$('li').css('background-color', 'white');
-		$(this).parent().parent().css('background-color', "blue");
+		$('#customButton').removeAttr('disabled');
+		$('#id_payment_level li').css('background-color', 'white');
+		$(this).parent().parent().css('background-color', "dodgerblue");
 	});
 
-	$('#stripeButton').click(function(e){
-		var handler = StripeCheckout.configure({
-		    key: 'pk_test_3aCn8J9IdHtqTUaCMWrABmQI',
-		    image: image,
-		    name: 'Inspection Type',
-		    panelLabel: 'Pay',
-		    token: function(token) {
-		      // Use the token to create the charge with a server-side script.
-		      // You can access the token ID with `token.id`
-		      $('inspection_select_form').append($('<input type="hidden" name="stripeToken" />').val(token));
-		      $('#inspection_select_form').submit();
-		    }
-		  });
-		value = $('input[name=payment_level]:checked', '#inspection_select_form').val(); 
+	// $('#customButton').click(function(e){
+	// 	var handler = StripeCheckout.configure({
+	// 	    key: 'pk_test_3aCn8J9IdHtqTUaCMWrABmQI',
+	// 	    image: image,
+	// 	    name: 'Inspection Type',
+	// 	    panelLabel: 'Pay',
+	// 	    token: function(token) {
+	// 	      // Use the token to create the charge with a server-side script.
+	// 	      // You can access the token ID with `token.id`
+	// 	      $('inspection_select_form').append($('<input type="hidden" name="stripeToken" />').val(token));
+	// 	      $('#inspection_select_form').submit();
+	// 	    }
+	// 	  });
+	// 	value = $('input[name=payment_level]:checked', '#inspection_select_form').val();
+	// 	var amount;
+	// 	var description;
+	// 	switch(value){
+	// 		case '1':
+	// 			amount = option1;
+	// 			description = "NoLemon Report";
+	// 			break;
+	// 		case '2':
+	// 			amount = option2;
+	// 			description = "Expert Car Condition Inspection Report and Video";
+	// 			break;
+	// 		case '3':
+	// 			amount = option3;
+	// 			description = "NoLemon + Expert Car Condition Inspection Report and Video (Value Pack)";
+	// 			break;
+	// 		default:
+	// 			alert('Something goofed');
+	// 			break;
+	// 	}
+	// 	handler.open({
+	//       description: description,
+	//       amount: amount
+	//     });
+	//     e.preventDefault();
 
+	//     $('#inspection_modal').fadeOut(500);
+	//     $('#inspection_modal').css('pointer-events', 'none');
+	// });
+
+	$('#customButton').click(function(){
+		var token = function(res){
+			var $input = $('<input type=hidden name=stripeToken />').val(res.id);
+			$('#inspection_select_form').append($input).submit();
+		};
+
+		value = $('input[name=payment_level]:checked', '#inspection_select_form').val();
 		var amount;
 		var description;
 		switch(value){
-			case '1':
+			case '29.99':
 				amount = option1;
 				description = "NoLemon Report";
 				break;
-			case '2':
+			case '37.00':
 				amount = option2;
 				description = "Expert Car Condition Inspection Report and Video";
 				break;
-			case '3':
+			case '56.00':
 				amount = option3;
 				description = "NoLemon + Expert Car Condition Inspection Report and Video (Value Pack)";
 				break;
 			default:
-				alert('Something goofed');
+				alert('There was an error, sorry!');
 				break;
 		}
-		handler.open({
-	      description: description,
-	      amount: amount
-	    });
-	    e.preventDefault();
 
-	    $('#inspection_modal').fadeOut("slow");
-	    $('#inspection_modal').css('pointer-events', 'none');
-	});
+		$('#inspection_modal').fadeOut(500);
+
+		email = $('input[name=email]', '#inspection_select_form').val();
+		StripeCheckout.open({
+			key:         'pk_test_3aCn8J9IdHtqTUaCMWrABmQI',
+			address:     false,
+			amount:      amount,
+			currency:    'cad',
+			name:        'NoLemon',
+			description: description,
+			panelLabel:  'Pay',
+			token:       token,
+			image: 		 image,
+			email: 		 email
+		});
+
+		return false;
+    });
 
 });
 
