b37b5a38dd6adc3e503e7bb5d710b3009404c93a
Solanar <and_555@hotmail.com>
2014-10-27 00:15:12 -0600

    up one level for base dir, switch to mysql

--- no_lemon/configs/prod_settings.py
+++ no_lemon/configs/prod_settings.py
@@ -2,15 +2,20 @@
 
 import os
 
-BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 
 #Change
 ALLOWED_HOSTS = ['nolemon.antyc.ca']
 
+#Change
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'nolemon',
+        'USER': 'django',
+        'PASSWORD': 'testdjangomysql',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
