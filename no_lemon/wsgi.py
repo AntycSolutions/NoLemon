9b26dacc640998eb83a5f5406501e5057ffdf14c
Solanar <and_555@hotmail.com>
2014-10-29 23:37:34 -0600

    generalize wsgi accessing project

--- no_lemon/wsgi.py
+++ no_lemon/wsgi.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 
 import os
 import sys
-sys.path.append('/home/airith/public_html/nolemon.antyc.ca/NoLemon')
+base_dir = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(base_dir)
 
 from django.core.wsgi import get_wsgi_application
 
